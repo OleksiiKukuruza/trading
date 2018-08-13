171c030317232a6d0fbc80b005d8df453f0a3f2c
Oleksii Kukuruza <oleksii.kukuruza@gmail.com>
2018-08-13 22:07:19 +0300

    add tests for getOrders util

---
+++ src/utils/getOrders.spec.js
@@ -0,0 +1,11 @@
+import getOrders from './getOrders';
+
+describe('getOrders', () => {
+  it('make fetch request on api url with page and and extracts json', async () => {
+    const result = { test: 'test' };
+    fetch.mockResponse(JSON.stringify(result));
+    const res = await getOrders(6);
+    expect(fetch).toHaveBeenCalledWith('http://localhost:5001/listOrders?start=6&size=100');
+    expect(res).toEqual(result);
+  });
+});
